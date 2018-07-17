0dd02f039f44d3921cdbcf35f0b33a49eb93543b
pivnicek <marc@keboola.com>
2018-07-17 18:24:25 +0200

    add clob tests

---
+++ src/test/java/com/keboola/tableexporter/ClobTest.java
@@ -0,0 +1,43 @@
+package com.keboola.tableexporter;
+
+import com.keboola.tableexporter.exception.ApplicationException;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertTrue;
+
+public class ClobTest extends BaseTest {
+
+    @Before
+    public void setupTables() throws Exception {
+        super.setupClobTable("clob/clobtest.csv", "clobtest");
+    }
+
+    @Test
+    public void testEscapingFile() throws IOException, URISyntaxException, ApplicationException
+    {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File expectedFile = new File(classLoader.getResource("clob/clobtest.csv").toURI());
+
+        Application app = new Application();
+
+        URI configUri = classLoader.getResource("clob/config.json").toURI();
+        String tmpConfig = createTemporaryConfigFile(Paths.get(configUri).toAbsolutePath().toString());
+        String[] args = {tmpConfig};
+        app.main(args);
+
+        File output = new File(outputFile);
+
+        assertTrue(
+            "Output file contents do not match expected",
+            FileUtils.contentEqualsIgnoreEOL(expectedFile, output, "UTF-8")
+        );
+    }
+}
