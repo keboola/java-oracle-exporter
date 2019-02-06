e624294968306c9f7f14658fd6e926bdf2ed8f82
pivnicek <marc@keboola.com>
2019-02-06 16:25:03 +0100

    Add getTables and TestConnection actions

--- src/main/java/com/keboola/tableexporter/Application.java
+++ src/main/java/com/keboola/tableexporter/Application.java
@@ -1,5 +1,6 @@
 package com.keboola.tableexporter;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,9 +12,15 @@ import java.util.Properties;
 import com.keboola.tableexporter.exception.ApplicationException;
 import com.keboola.tableexporter.exception.CsvException;
 import com.keboola.tableexporter.exception.UserException;
+import com.sun.jna.platform.win32.Netapi32Util;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Application {
+
+    public final static String OUTPUT_DIR = "output/";
+
+    private static String action;
     private static String dbPort;
     private static String dbHost;
     private static String dbUser;
@@ -90,7 +97,7 @@ public class Application {
                 }
             }
             String[] headerArr = new String[header.size()];
-            CsvWriter writer = new CsvWriter(outputFile, (includeHeader) ? header.toArray(header.toArray(headerArr)) : null);
+            CsvWriter writer = new CsvWriter(OUTPUT_DIR + outputFile, (includeHeader) ? header.toArray(headerArr) : null);
             // write the result set to csv
             int rowCount = writer.write(rs, hasLobs);
             final long end = System.nanoTime();
@@ -103,16 +110,156 @@ public class Application {
             throw new UserException("IO Exception: " + ex.getMessage(), ex);
         }
     }
+
+    private static void getTables() throws UserException {
+        System.out.println("Fetching table listing");
+        try {
+            Statement stmt = connection.createStatement();
+            String tableListQuery = "SELECT TABS.TABLE_NAME ,\n" +
+                    "    TABS.TABLESPACE_NAME ,\n" +
+                    "    TABS.OWNER ,\n" +
+                    "    TABS.NUM_ROWS ,\n" +
+                    "    COLS.COLUMN_NAME ,\n" +
+                    "    COLS.DATA_LENGTH ,\n" +
+                    "    COLS.DATA_PRECISION ,\n" +
+                    "    COLS.DATA_SCALE ,\n" +
+                    "    COLS.COLUMN_ID ,\n" +
+                    "    COLS.DATA_TYPE ,\n" +
+                    "    COLS.NULLABLE ,\n" +
+                    "    REFCOLS.CONSTRAINT_NAME ,\n" +
+                    "    REFCOLS.CONSTRAINT_TYPE ,\n" +
+                    "    REFCOLS.INDEX_NAME ,\n" +
+                    "    REFCOLS.R_CONSTRAINT_NAME,\n" +
+                    "    REFCOLS.R_OWNER\n" +
+                    "FROM ALL_TAB_COLUMNS COLS\n" +
+                    "    JOIN\n" +
+                    "    (\n" +
+                    "        SELECT \n" +
+                    "        TABLE_NAME , \n" +
+                    "        TABLESPACE_NAME, \n" +
+                    "        OWNER , \n" +
+                    "        NUM_ROWS\n" +
+                    "        FROM all_tables\n" +
+                    "        WHERE all_tables.TABLESPACE_NAME != 'SYSAUX'\n" +
+                    "        AND all_tables.TABLESPACE_NAME != 'SYSTEM'\n" +
+                    "        AND all_tables.OWNER != 'SYS'\n" +
+                    "        AND all_tables.OWNER != 'SYSTEM'\n" +
+                    "    )\n" +
+                    "    TABS\n" +
+                    "        ON COLS.TABLE_NAME = TABS.TABLE_NAME\n" +
+                    "        AND COLS.OWNER = TABS.OWNER\n" +
+                    "    LEFT OUTER JOIN\n" +
+                    "    (\n" +
+                    "        SELECT ACC.COLUMN_NAME ,\n" +
+                    "        ACC.TABLE_NAME ,\n" +
+                    "        AC.CONSTRAINT_NAME ,\n" +
+                    "        AC.R_CONSTRAINT_NAME,\n" +
+                    "        AC.INDEX_NAME ,\n" +
+                    "        AC.CONSTRAINT_TYPE ,\n" +
+                    "        AC.R_OWNER\n" +
+                    "        FROM ALL_CONS_COLUMNS ACC\n" +
+                    "            JOIN ALL_CONSTRAINTS AC\n" +
+                    "                ON ACC.CONSTRAINT_NAME = AC.CONSTRAINT_NAME\n" +
+                    "        WHERE AC.CONSTRAINT_TYPE IN ('P', 'U', 'R')\n" +
+                    "    )\n" +
+                    "    REFCOLS ON COLS.TABLE_NAME = REFCOLS.TABLE_NAME\n" +
+                    "        AND COLS.COLUMN_NAME = REFCOLS.COLUMN_NAME\n" +
+                    "SQL;";
+            ResultSet resultSet = stmt.executeQuery(tableListQuery);
+            JSONObject output = new JSONObject();
+            while(resultSet.next()) {
+                String curTable = resultSet.getString("OWNER") + "." + resultSet.getString("TABLE_NAME");
+                if (!output.has(curTable)) {
+                    JSONObject tableData = new JSONObject();
+                    tableData.put("name", resultSet.getString("TABLE_NAME"));
+                    tableData.put("tablespaceName", resultSet.getString("TABLESPACE_NAME"));
+                    tableData.put("schema", resultSet.getString("OWNER"));
+                    tableData.put("owner", resultSet.getString("OWNER"));
+                    if (resultSet.getString("NUM_ROWS") != null) {
+                        tableData.put("rowCount", resultSet.getInt("NUM_ROWS"));
+                    }
+                    output.put(curTable, tableData);
+                }
+
+                JSONObject curObject = output.getJSONObject(curTable);
+                if (!curObject.has("columns")) {
+                    curObject.put("columns", new JSONObject());
+                }
+                JSONObject curColumns = curObject.getJSONObject("columns");
+                String curColumnIndex = String.valueOf(resultSet.getInt("COLUMN_ID") - 1);
+                if (!curColumns.has(curColumnIndex)) {
+                    JSONObject columnData = new JSONObject();
+                    String length = resultSet.getString("DATA_LENGTH");
+                    if (resultSet.getString("DATA_PRECISION") != null
+                            && resultSet.getString("DATA_SCALE") != null)
+                    {
+                        length = resultSet.getString("DATA_PRECISION")
+                                + "," + resultSet.getString("DATA_SCALE");
+                    }
+                    columnData.put("name", resultSet.getString("COLUMN_NAME"));
+                    columnData.put("type", resultSet.getString("DATA_TYPE"));
+                    columnData.put("nullable", resultSet.getString("NULLABLE") == "Y" ? true : false);
+                    columnData.put("length", length);
+                    columnData.put("ordinalPosition", resultSet.getInt("COLUMN_ID"));
+                    columnData.put("primaryKey", false);
+                    columnData.put("uniqueKey", false);
+                    curColumns.put(curColumnIndex, columnData);
+                    curObject.put("columns", curColumns);
+                }
+                JSONObject currentColumn = curColumns.getJSONObject(curColumnIndex);
+                switch (resultSet.getString("CONSTRAINT_TYPE")) {
+                    case "R":
+                        currentColumn.put("foreignKeyName", resultSet.getString("CONSTRAINT_NAME"));
+                        currentColumn.put("foreignKeyTable", resultSet.getString("R_OWNER"));
+                        currentColumn.put("foreignKeyRef", resultSet.getString("$_CONSTRAINT_NAME"));
+                        break;
+                    case "P":
+                        currentColumn.put("primaryKey", true);
+                        currentColumn.put("primaryKeyName", resultSet.getString("CONSTRAINT_NAME"));
+                        break;
+                    case "U":
+                        currentColumn.put("uniqueKey", true);
+                        currentColumn.put("uniqueKeyName", resultSet.getString("CONSTRAINT_NAME"));
+                        break;
+                    default:
+                        break;
+                }
+                curColumns.put(curColumnIndex, currentColumn);
+                curObject.put("columns", curColumns);
+                output.put(curTable, curObject);
+            }
+            try (FileWriter file = new FileWriter(outputFile)) {
+                file.write(output.toString());
+                System.out.println("Successfully Copied JSON Table Listing to File...");
+            } catch (IOException ioException) {
+                throw new UserException("IO Exception: " + ioException.getMessage(), ioException);
+            }
+        } catch (SQLException sqlException) {
+            throw new UserException("SQL Exception: " + sqlException.getMessage(), sqlException);
+        }
+    }
     
     public static void main(String[] args) {
         try {
-            readConfigFile(args[0]);
-            includeHeader = true;
-            if (args.length > 1) {
-                includeHeader = Boolean.parseBoolean(args[1]);
+            action = args[0];
+            readConfigFile(args[1]);
+            switch (action) {
+                case "testConnection":
+                    connectDb();
+                    break;
+                case "getTables":
+                    connectDb();
+                    getTables();
+                    break;
+                default:
+                    includeHeader = true;
+                    if (args.length > 2) {
+                        includeHeader = Boolean.parseBoolean(args[2]);
+                    }
+                    connectDb();
+                    fetchData();
+                    break;
             }
-            connectDb();
-            fetchData();
             System.out.println("All done");
         } catch (UserException ex) {
             System.err.println(ex.getMessage());
