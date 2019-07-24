cd6322bcb861cf81a9729ab679341bfec615b0f3
pivnicek <marc@keboola.com>
2019-07-24 17:50:42 +0200

    add test for single table no columns

--- src/test/java/com/keboola/tableexporter/GetTablesTest.java
+++ src/test/java/com/keboola/tableexporter/GetTablesTest.java
@@ -70,4 +70,23 @@ public class GetTablesTest extends BaseTest {
 
         assertTrue("The files differ!", FileUtils.contentEqualsIgnoreEOL(expectedFile, output, "UTF-8"));
     }
+
+    @Test
+    public void testGetSingleTableNoColumns() throws IOException, URISyntaxException, ApplicationException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File expectedFile = new File(classLoader.getResource("getTables/expectedSingleTableNoColumnsResults.json").toURI());
+
+        Application app = new Application();
+
+        URI configUri = classLoader.getResource("getTables/singleTableNoColumnsConfig.json").toURI();
+
+        String tmpFile = super.createTemporaryConfigFile(Paths.get(configUri).toAbsolutePath().toString());
+
+        String[] args = {"getTables", tmpFile};
+        app.main(args);
+
+        File output = new File("tables.json");
+
+        assertTrue("The files differ!", FileUtils.contentEqualsIgnoreEOL(expectedFile, output, "UTF-8"));
+    }
 }
